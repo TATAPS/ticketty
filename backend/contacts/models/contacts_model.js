675a3298de5d1f5c5303c389b02f6fcee16d62e4
Tim Lynch/Janssen <tsplynch@gmail.com>
2024-01-11 12:31:14 -0500

    setup backend, corrected some mysql mistakes and adjusted package.json for backend

---
+++ backend/contacts/models/contacts_model.js
@@ -0,0 +1,12 @@
+const connectDatabase = require("./db_connnection.js");
+
+async function getAllContacts() {
+  const query = "SELECT * from contacts;";
+  const db = await connectDatabase();
+  const [contacts] = await db.query(query);
+  return contacts;
+}
+
+module.exports = {
+  getAllContacts,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 
+const { router: contactsRouter } = require("./contacts/index.js");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/contacts", contactsRouter);
 app.get("/", (req, res, next) => {
   res.send("Hello from backend");
 });
