a3837929be9454caa5818ed42827531a67cebb19
Trang Nguyen <ntttrang2130@gmail.com>
2024-01-18 10:48:21 -0500

    update css

--- frontend/src/auth/Auth.jsx
+++ frontend/src/auth/Auth.jsx
@@ -7,28 +7,29 @@ function Auth() {
   const [active, setActive] = useState(false)
 
   return (
-    <div id="container" className={active ? "container active" : "container"}>
-      {/* SIGN_IN */}
-      <Login />
-      {/* SIGN_UP */}
-      <Register />
-      {/* TOGGLE_CONTAINER */}
-      <div className="toggle-container">
-        <div className="toggle">
-          <div className="toggle-panel toggle-left">
-            <h1>Welcome Back!</h1>
-            <p>Enter your personal details to use all of site features</p>
-            <button className="hidden" id="login" onClick={() => setActive(false)}>Sign In</button>
-          </div>
-          <div className="toggle-panel toggle-right">
-            <h1>Hello!</h1>
-            <p>Register with your personal details to use all of site features</p>
-            <button className="hidden" id="register" onClick={() => setActive(true)}>Sign Up</button>
+    <div className='auth-container'>
+      <div id="container" className={active ? "container active" : "container"}>
+        {/* SIGN_IN */}
+        <Login />
+        {/* SIGN_UP */}
+        <Register />
+        {/* TOGGLE_CONTAINER */}
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button className="hidden" id="login" onClick={() => setActive(false)}>Sign In</button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Hello!</h1>
+              <p>Register with your personal details to use all of site features</p>
+              <button className="hidden" id="register" onClick={() => setActive(true)}>Sign Up</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
   )
 }
 
