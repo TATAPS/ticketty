287c0bf55120422be97888e87fbd77648a218972
Trang Nguyen <ntttrang2130@gmail.com>
2024-03-13 22:56:37 -0400

    test prod2

--- backend/dev_scripts/db_seed.js
+++ backend/dev_scripts/db_seed.js
@@ -9,10 +9,10 @@ async function starterSeedDatabase() {
     });
 
     const connection = await mysql.createConnection({
-      host: process.env.HOST,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.SEEDDATABASE,
+      host: process.env.MYSQLHOST,
+      user: process.env.MYSQLUSER,
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       multipleStatements: true,
     });
 
