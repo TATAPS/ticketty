1fe34daf8bddbb56f401fcc673e0fcd796bb4ae2
Trang Nguyen <ntttrang2130@gmail.com>
2024-03-14 01:11:30 -0400

    test prod10

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8080;
 
 const init = async () => {
   starterSeedDatabase();
-  app.listen(PORT, () => console.log(`Listening at https://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 };
 
 init();
